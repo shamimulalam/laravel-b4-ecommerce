8ecae85022ed7acbb1ff64d4079d3a25bac5d844
Trainer PNT <trainer@piit.local>
2019-11-24 20:03:57 +0600

    Transaction migratin done

---
+++ database/migrations/2019_11_24_135331_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('order_id');
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->float('amount',10,2);
+            $table->string('card_type');
+            $table->dateTime('transaction_date');
+            $table->text('transaction_data');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
